f6eb636ae7d262f7dbc36ed8d79a579c041351ab
neoOkpara <91058786+enwabudo@users.noreply.github.com>
2021-11-25 22:07:12 +0100

    Setup the project

---
+++ src/main/java/io/core/libra/LibraApplication.java
@@ -0,0 +1,13 @@
+package io.core.libra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/core/libra/LibraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.core.libra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
